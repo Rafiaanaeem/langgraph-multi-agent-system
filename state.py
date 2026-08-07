d5ed0ff497ff6d0f00c988c2c2682c243b4f381b
RAFIA NAEEM <220523495+Rafiaanaeem@users.noreply.github.com>
2026-08-07 12:08:33 +0500

    define AgentState, SubTaskState, and manage_list reducer to handle global memory, isolated parallel worker contexts, and safe data clearing between stages

--- state.py
+++ state.py
@@ -1,15 +1,30 @@
-from typing import Annotated, TypedDict, Any, Optional
-from langgraph.graph.message import add_messages
+from typing import Annotated, List, TypedDict, Dict, Optional
+import operator
 from langchain_core.messages import BaseMessage
+def manage_list(left: list, right: list):
+    if not right:
+        return left
+
+    if isinstance(right, list) and right and right[0] == "CLEAR":
+        return []
+
+    return left + right
 
 class AgentState(TypedDict):
-    """
-    The shared memory structure for our Multi-Agent LangGraph.
-    Every node will receive this state and return updates to it.
-    """
-    messages: Annotated[list[BaseMessage], add_messages]
-    agent_queue: list[str]
-    next_node: str
-    current_agent: str
-    last_agent: str 
-    files: Optional[list[Any]]
+    messages: Annotated[List[BaseMessage], operator.add]
+    files: Optional[List[bytes]]
+    plan: List[List[Dict[str, str]]]
+    current_stage: int
+    context: str
+    step_results: Annotated[List[str], manage_list]
+    final_outputs: Annotated[List[Dict[str, str]], manage_list]
+    last_agent: str
+
+
+class SubTaskState(TypedDict):
+    messages: List[BaseMessage]
+    context: str
+    query: str
+    agent_name: str
+    is_final: bool
+    last_agent: str
